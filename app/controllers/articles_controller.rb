1e8e7d001dbcc1a211d83af6d2d3630cfdf4c38e
yasser lamalem <yasserlamalem@gmail.com>
2016-02-16 23:13:41 +0000

    salina edit o new o dak chi

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,7 +37,12 @@ class ArticlesController < ApplicationController
      render 'edit'
    end
  end
-    
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted!!"
+    redirect_to articles_path 
+  end
   private
   def articles_params
     params.require(:article).permit(:title,:description)
