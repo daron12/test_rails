59857ec652ea9a05ef05b7a8a8b174457acaf2e5
yasser lamalem <yasserlamalem@gmail.com>
2016-02-16 22:45:17 +0000

    naçssa destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
 class ArticlesController < ApplicationController
-  def new 
+  def index
+    @articles = Article.all
+  end
+  
+  def new
+     
     @article = Article.new
   end
   def create
@@ -18,6 +23,21 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
+  def edit 
+    @article = Article.find(params[:id])
+  end
+ 
+ def update
+   @article = Article.find(params[:id])
+  
+   if @article.update(articles_params) 
+     flash[:notice] = "Successfully Updated"
+     redirect_to article_path(@article)
+   else
+     render 'edit'
+   end
+ end
+    
   private
   def articles_params
     params.require(:article).permit(:title,:description)
