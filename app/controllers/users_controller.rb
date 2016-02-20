066a0990c2e2aecff4573b64d76baabbefe781b6
yasser lamalem <yasserlamalem@gmail.com>
2016-02-20 01:21:50 +0000

    signup to login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(users_params)
     if @user.save
+      session[:user_id]=@user.id
       flash[:success] = "Welcome to my test application #{@user.username}"
-      redirect_to  articles_path
+      redirect_to  user_path(@user)
     else
       render 'new'
     end
