b7358a2626a65b23b6c9967a0ee507ef08dd774b
yasser lamalem <yasserlamalem@gmail.com>
2016-02-22 22:46:59 +0000

    update all wiews with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,7 +8,8 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
   def show
-    
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page],per_page:5)
   end
   def create
     @category = Category.new(category_params)
