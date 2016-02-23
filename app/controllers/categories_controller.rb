8b5de835a561af4d0bbb53e7683526bc4f6ec5c8
yasser lamalem <yasserlamalem@gmail.com>
2016-02-23 01:33:45 +0000

    add edit category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,6 +20,19 @@ class CategoriesController < ApplicationController
     render 'new'  
     end
   end
+  def edit
+    @category = Category.find(params[:id])
+  end
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+    flash[:success] = "Successfully updated"
+    redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+    
+  end
   private
   def category_params
     params.require(:category).permit(:name)
